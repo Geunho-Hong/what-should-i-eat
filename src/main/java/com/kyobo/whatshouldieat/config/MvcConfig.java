a84d435007e0d5591e140c81e77e4aaefe01c5bb
GeunhoHong <hong8159@kyobobook.com>
2023-04-06 09:34:10 +0900

    Feature(13265/홍근호) : 슬랙 알람 연동 테스트

---
+++ src/main/java/com/kyobo/whatshouldieat/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.kyobo.whatshouldieat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfig {
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+
+}
